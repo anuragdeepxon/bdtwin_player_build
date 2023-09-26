13828db948f5c3cacc91bd1cac868c2b911aa80f
Anurag Deep <anuragdeep.xon@gmail.com>
2023-09-26 18:21:39 +0530

    build

---
+++ assets/Transactions-ab35ebe8.js
@@ -0,0 +1,13 @@
+import{s as a,r as t,_ as n,j as s,T as o}from"./index-3e69953a.js";const i=a.section`
+  padding: 0 15px;
+`,e=a.div`
+  max-width: 1124px;
+  width: 100%;
+  padding: 24px 30px 106px 30px;
+  margin: 0 auto;
+  position: relative;
+  margin-top: 60px;
+  margin-bottom: 60px;
+  background-color: #151515;
+  border-radius: 8px;
+`,p=t.lazy(()=>n(()=>import("./TransactionsTable-ece82fbb.js"),["assets/TransactionsTable-ece82fbb.js","assets/index-3e69953a.js","assets/Pagination.styles-a67f58ad.js","assets/Pagination-17e446de.js"])),x=r=>s.jsx(t.Suspense,{fallback:null,children:s.jsx(p,{...r})}),l=()=>s.jsx(i,{children:s.jsxs(e,{children:[s.jsx(o,{title:"TRANSACTIONS"}),s.jsx(x,{})]})});export{l as default};
