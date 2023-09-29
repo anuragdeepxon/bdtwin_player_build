f1bb9aed2922e6bc7e6541a9ae2449ef974a8770
Anurag Deep <anuragdeep.xon@gmail.com>
2023-09-29 18:47:25 +0530

    API Integration

--- assets/About-ad5f4ea1.js
+++
@@ -1,1 +0,0 @@
-import{s as e,j as s,L as t,O as r}from"./index-6ea86406.js";const n=e.section``,i=()=>s.jsxs(n,{children:[s.jsx("h1",{children:"About"}),s.jsxs("ul",{children:[s.jsx("li",{children:s.jsx(t,{to:"contacts",children:"Our Contact"})}),s.jsx("li",{children:s.jsx(t,{to:"team",children:"Our Team"})})]}),s.jsx(r,{})]});export{i as default};
