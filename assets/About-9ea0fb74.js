2500325af3db7659e983a556663a3e42fc283842
Anurag Deep <anuragdeep.xon@gmail.com>
2023-10-08 00:17:32 +0530

    update

---
+++ assets/About-9ea0fb74.js
@@ -0,0 +1,1 @@
+import{s as e,j as s,L as t,O as r}from"./index-f6a63d74.js";const n=e.section``,i=()=>s.jsxs(n,{children:[s.jsx("h1",{children:"About"}),s.jsxs("ul",{children:[s.jsx("li",{children:s.jsx(t,{to:"contacts",children:"Our Contact"})}),s.jsx("li",{children:s.jsx(t,{to:"team",children:"Our Team"})})]}),s.jsx(r,{})]});export{i as default};
---
+++ assets/Transactions-eb088904.js
@@ -0,0 +1,13 @@
+import{s as a,r as t,_ as n,j as s,T as o}from"./index-f6a63d74.js";const i=a.section`
+  padding: 0 15px;
+`,e=a.div`
+  max-width: 1124px;
+  width: 100%;
+  padding: 24px 30px 106px 30px;
+  margin: 0 auto;
+  position: relative;
+  margin-top: 60px;
+  margin-bottom: 60px;
+  background-color: #151515;
+  border-radius: 8px;
+`,p=t.lazy(()=>n(()=>import("./TransactionsTable-7d697bff.js"),["assets/TransactionsTable-7d697bff.js","assets/index-f6a63d74.js"])),x=r=>s.jsx(t.Suspense,{fallback:null,children:s.jsx(p,{...r})}),l=()=>s.jsx(i,{children:s.jsxs(e,{children:[s.jsx(o,{title:"TRANSACTIONS"}),s.jsx(x,{})]})});export{l as default};
